45de3715ec795456bf48550f8f319c35ff47c768
Young Rok Kim <yk@kaapke.com>
2025-02-23 15:22:48 +0100

    headless setup

--- cms/site/config/config.php
+++ cms/site/config/config.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-    'debug' => true,
+
+
+
+
+
 ];
---
+++ frontend/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const { data, refresh, error, status, clear } = await useKql({
+  query: 'site',
+  select: {
+    title: true,
+    children: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1>{{ data?.result?.title }}</h1>
+    <pre>{{ JSON.stringify(data?.result, undefined, 2) }}</pre>
+  </div>
+</template>
